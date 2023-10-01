64623d37463cafc61d21b1e08e0f573f2a8ce470
VictoriaPresnyakova <victoriapresnyakova202@gmail.com>
2023-10-01 15:54:07 +0400

    done

--- ReadCSV.py
+++ ReadCSV.py
@@ -1,32 +1,48 @@
 import io
+
+import numpy
+import numpy as np
 import pandas as pd
-from numpy import ndarray
 from Try_parse_int import try_parse_int
 
 
-def filtered_csv_file(columns_from: str, columns_to: str, rows_from: str, rows_to: str) -> tuple:
-    data = pd.read_csv(
-        './data/car_price_prediction.csv',
-        delimiter=',')
-    row_len, col_len = data.shape
+class MyCSV:
+    def __init__(self):
+        self.data = pd.read_csv(
+            './data/car_price_prediction.csv',
+            delimiter=',')
 
-    columns_from = try_parse_int(columns_from, 1) - 1
-    columns_to = try_parse_int(columns_to, col_len)
+    def filtered_csv_file(self, columns_from: str, columns_to: str, rows_from: str, rows_to: str) -> tuple:
+        row_len, col_len = self.data.shape
 
-    rows_from = try_parse_int(rows_from, 1) - 1
-    rows_to = try_parse_int(rows_to, row_len)
+        columns_from = try_parse_int(columns_from, 1) - 1
+        columns_to = try_parse_int(columns_to, col_len)
 
-    data = data.iloc[rows_from if rows_from >= 0 else 0:
-                     rows_to if rows_from < rows_to < row_len else row_len,
-                     columns_from if columns_from >= 0 else 0:
-                     columns_to if columns_from < columns_to < col_len else col_len]
+        rows_from = try_parse_int(rows_from, 1) - 1
+        rows_to = try_parse_int(rows_to, row_len)
 
-    row_titles = data.columns.tolist()
+        data = self.data.iloc[rows_from if rows_from >= 0 else 0:
+                              rows_to if rows_from < rows_to < row_len else row_len,
+               columns_from if columns_from >= 0 else 0:
+               columns_to if columns_from < columns_to < col_len else col_len]
 
-    content = data.to_numpy()
+        row_titles = data.columns.tolist()
 
-    buffer = io.StringIO()
-    data.info(buf=buffer)
-    info = buffer.getvalue()
+        content = data.to_numpy()
 
-    return content, row_titles, info.split('\n')
+        buffer = io.StringIO()
+        data.info(buf=buffer)
+        info = buffer.getvalue()
+
+        return content, row_titles, info.split('\n')
+
+    def statistics(self, column_to_group: str) -> tuple:
+        columns_to_show = ['Price']
+        first_column = sorted(self.data[column_to_group].unique())
+
+        data = self.data.groupby([column_to_group])[columns_to_show].agg([np.min, np.max, np.average])
+        row_titles = [''] + data.columns.tolist()
+        data = data.to_numpy()
+        new_column = np.array(first_column).reshape(-1, 1)
+        result_matrix = np.hstack((new_column, data))
+        return result_matrix, row_titles
--- WebApp.py
+++ WebApp.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, escape, session
-import pandas as pd
-from ReadCSV import filtered_csv_file
 
+from ReadCSV import MyCSV
 
 app = Flask(__name__)
+my_CSV = MyCSV()
 
 
 @app.route('/')
@@ -14,10 +14,19 @@ def entry_page() -> 'html':
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
-    content, titles, info = filtered_csv_file(request.form['columns_from'], request.form['columns_to'],
-                             request.form['rows_from'], request.form['rows_to'])
+    content, titles, info = my_CSV.filtered_csv_file(request.form['columns_from'], request.form['columns_to'],
+                                                     request.form['rows_from'], request.form['rows_to'])
     return render_template('viewlog.html', the_title="Data",
                            the_data=content, the_row_titles=titles, info=info)
+
+
+@app.route('/statistics', methods=['POST'])
+def do_statistics() -> 'html':
+    option = request.form['option']
+    result_matrix, row_titles = my_CSV.statistics(option)
+
+    return render_template('viewlog.html', the_title=option,
+                           the_data=result_matrix, the_row_titles=row_titles)
 
 
 if __name__ == '__main__':
