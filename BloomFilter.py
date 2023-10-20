6267ddaacd21a6281529e8f5b4f8144c0a0c4f85
VictoriaPresnyakova <victoriapresnyakova202@gmail.com>
2023-10-20 16:03:04 +0400

    done

---
+++ BloomFilter.py
@@ -0,0 +1,32 @@
+import math
+from bitarray import bitarray
+
+class BloomFilter(object):
+
+    def __init__(self, size, number_expected_elements=100000):
+        self.size = size
+        self.number_expected_elements = number_expected_elements
+
+        self.bloom_filter = bitarray(self.size)
+        self.bloom_filter.setall(0)
+
+        self.number_hash_functions = round((self.size / self.number_expected_elements) * math.log(2))
+
+    def _hash_djb2(self, s):
+        hash = 5381
+        for x in s:
+            hash = ((hash << 5) + hash) + ord(x) # умножение hash на 33
+        return hash % self.size
+
+    def _hash(self, item, K):
+        return self._hash_djb2(str(K) + item)
+
+    def add_to_filter(self, item):
+        for i in range(self.number_hash_functions):
+            self.bloom_filter[self._hash(item, i)] = 1
+
+    def check_is_not_in_filter(self, item):
+        for i in range(self.number_hash_functions):
+            if self.bloom_filter[self._hash(item, i)] == 0: # элемента нет в наборе
+                return True
+        return False # мы не можем сказать что элемента нет в наборе
--- WebApp.py
+++ WebApp.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template, request, escape, session
 
+from BloomFilter import BloomFilter
 from ReadCSV import MyCSV
 
 app = Flask(__name__)
 my_CSV = MyCSV()
+bloom_filter = BloomFilter(1000000, 100000)
+
+v2 = 'age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall,output'.lower()
+
+v7 = ('stock index; country; year; index price; log_indexprice; inflationrate; oil prices; exchange_rate; '
+      'gdppercent; percapitaincome; unemploymentrate; manufacturingoutput; tradebalance; USTreasury; NASDAQ; '
+      'FTSE 100; Nifty 50; Nikkei 225; HSI; SZCOMP; DAX 30; CAC 40; IEX 35; United States of America; United Kingdom; '
+      'India; Japan; Hong Kong; China; Germany; France; Spain').lower()
+
+v17 = ('Price; Levy; Manufacturer; Model; year; Category; Leather interior; Fuel type; Engine volume; Mileage; '
+       'Cylinders; Gear box type; Drive wheels; Doors; Wheel; Color; Airbags').lower()
+
+unique_titles = 'Manufacturer; Model; Category; Gear box type; Drive wheels; Doors; Wheel; Color'
+values = v17.split('; ')
+values.extend([j for i in unique_titles.split('; ') for j in my_CSV.data[i].unique()])
+
+kaggles = {'https://www.kaggle.com/datasets/deepcontractor/car-price-prediction-challenge':
+           list(map(lambda x: x.lower(), values)),
+           'https://www.kaggle.com/datasets/pratik453609/economic-data-9-countries-19802020': v7.split('; '),
+           'https://www.kaggle.com/datasets/rashikrahmanpritom/heart-attack-analysis-prediction-dataset?select=heart.csv': v2.split(',')}
+
+for v in kaggles.values():
+    for i in v:
+        bloom_filter.add_to_filter(i.lower())
 
 
 @app.route('/')
@@ -30,6 +55,18 @@ def do_statistics() -> 'html':
 def add_rows():
     my_CSV.add_data()
     return do_search()
+
+
+@app.route('/search4kaggle', methods=['POST'])
+def search4kaggle():
+    word = request.form['searchKaggle'].lower()
+    print(word)
+    if not bloom_filter.check_is_not_in_filter(word):  # it can be in list
+        lst = [k for k, val in kaggles.items() if word in val]
+        print(lst)
+        if lst:
+            return render_template('viewlog.html', the_title="Links", info=lst)
+    return entry_page()
 
 
 if __name__ == '__main__':
